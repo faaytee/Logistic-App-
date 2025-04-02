749c200ed21a9b42f16e7c3e37f2e57cb3a02678
faaytee <faithbassey020@gmail.com>
2025-04-02 20:12:13 +0100

    initial commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController.js");
+const {
+  register,
+  login,
+  deliveryRequest,
+} = require("../controllers/authController.js");
 
 const router = express.Router();
 
 router.post("/auth/register", register);
 router.post("/auth/login", login);
+router.post("/request", deliveryRequest);
 
 module.exports = router;

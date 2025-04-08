77cdcbde507e83dd93115fbff9c39e7cb9320060
Adeyemo Stephen <87942124+stivex001@users.noreply.github.com>
2025-04-08 12:43:52 +0100

    Update server.js
    Signed-off-by: Adeyemo Stephen <87942124+stivex001@users.noreply.github.com>

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 
 const authRoutes = require("./routes/authRoutes.js");
+const deliveryRoutes = require("./routes/deliveryRoutes.js");
+const driverRoutes = require("./routes/driverRoutes.js");
 const errorHandler = require("./middleware/errorHandler.js");
 
 const prisma = new PrismaClient();
@@ -12,11 +14,15 @@ const app = express();
 
 app.use(express.json());
 
+
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
 app.use("/api", authRoutes);
+app.use("/api", deliveryRoutes);
+app.use("/api", driverRoutes);
 app.use(errorHandler);
 
 app.listen(port, () => {

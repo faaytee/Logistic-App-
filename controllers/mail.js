4d06fe27de438deb89f9446cc73e8df2aa6bf6a9
faaytee <faithbassey020@gmail.com>
2025-04-10 09:25:24 +0100

    Update: added assigned deliveries logic

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,24 +1,36 @@
 const nodemailer = require("nodemailer");
-//const SMTPTransport = require("nodemailer/lib/smtp-transport");
+require("dotenv").config();
 
-//async function sendMail() {
-const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: Process.env.MAIL_PORT,
-  Secure: process.env.NODE_ENV, //true
-  auth: {
-    user: process.env.MAIL_USER,
-    password: process.env.MAIL_PASSWORD,
-  },
-}); //as SMTPTransport.Options);
-//}
-export const sendEmail = async (dto) => {
-  const { sender, recepient, subject, message } = dto;
-  return await transporter.sendMail({
-    from: sender,
-    to: recepient,
-    subject,
-    //html: message,
-    text: message,
-  });
-};
+// // const html = `
+// // <h1> Hi there</h1>
+// // <p> trying out nodemailer to send mails</P>
+// // `;
+
+// //const email = ["bassseynancy2@gmail.com", "stephenadeyemo@gmail.com"];
+
+async function sendMail(toEmail, subject, htmlContent) {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+    const info = await transporter.sendMail({
+      from: '"Faith" <faithbassey020@gmail.com>',
+      to: toEmail,
+      subject: subject,
+      html: htmlContent,
+    });
+    console.log(`email sent to: ${toEmail}`);
+    console.log(`message id: ${info.messageId} `);
+  } catch (error) {
+    console.error(`error sending email to: ${toEmail}`, error);
+    throw new Error(`failed to send email`);
+  }
+}
+
+module.exports = { sendMail };
